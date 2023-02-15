abe7b3e3f9ba18d053973bf3c3f473be40e4c9af
J-Barta <jacob.barta25@gmail.com>
2023-02-15 17:55:48 -0500

    team

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import KeyValueTable from "./components/general-components/KeyValueTable";
 import useEntry from "./networktables/useEntry";
 
 function App() {
-  const [time] = useEntry("/dashboard/time-left", 0.0)
+  const [time] = useEntry("/dashboard/time-left", 0.0);
+  const [team] = useEntry("/dashboard/team", "FAILED TO GET TAM")
 
   return (
     <div>
       <div>
         <h1 style={{color: "white"}}>{time}</h1>
+        <h1 style={{color: "white"}}>{team}</h1>
       </div>
       <AutonomousSelector />
     </div>
