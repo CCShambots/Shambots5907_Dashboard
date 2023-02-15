3c4e48b006fe49325986deb869a7e6b5a4ce5b50
J-Barta <jacob.barta25@gmail.com>
2023-02-15 17:52:08 -0500

    dashboard done forever, never being touched agaiun for sure

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,16 @@
-import { useState } from 'react';
-import KeyValueTable from './components/general-components/KeyValueTable';
-import useGlobalListener from './networktables/useGlobalListener';
-import useEntry from './networktables/useEntry';
-import ConnectionWarning from './components/pages/ConnectionWarning.jsx';
-import React, {Component} from 'react';
-import './App.css';
-import Ribbon from './components/pages/Ribbon';
-import PageAutonomous from './components/pages/PageAutonomous';
-import PageTeleop from './components/pages/PageTeleop';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Debug from "./components/pages/Debug";
-import PurePursuit from "./components/pages/PurePursuit";
-
+import AutonomousSelector from "./components/specific-components/AutonomousSelector";
+import KeyValueTable from "./components/general-components/KeyValueTable";
+import useEntry from "./networktables/useEntry";
 
 function App() {
-
-  const [tableEntries, setTableEntries] = useState({});
-  useGlobalListener((key, value) => {
-    setTableEntries(previousValue => ({
-      ...previousValue,
-      [key]: value,
-    }));
-  }, true);
-
-  const [activeTab, setActiveTab] = useEntry('/SmartDashboard/Tab', 'Auto');
-
-  const tabNames = ['Auto', 'Teleop', 'Test', 'Debug', 'Pathing'];
+  const [time] = useEntry("/dashboard/time-left", 0.0)
 
   return (
-    <div className="App">
-      
-        <ConnectionWarning/>
-
-        <Ribbon tab={activeTab} setTab={(tab) => setActiveTab(tab)} tabs={tabNames}/>
-
-        <PageAutonomous activeTab={activeTab}/>
-        <PageTeleop activeTab={activeTab}/>
-        <Debug activeTab={activeTab}></Debug>
-        <PurePursuit activeTab={activeTab}></PurePursuit>
+    <div>
+      <div>
+        <h1 style={{color: "white"}}>{time}</h1>
+      </div>
+      <AutonomousSelector />
     </div>
   );
 }
